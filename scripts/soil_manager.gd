4ab337064dc9179e173701e1b6861e3b11daf717
Anishax11 <anishabhosale11@gmail.com>
2025-06-14 23:46:28 +0530

    added paralax background

--- scripts/soil_manager.gd
+++ scripts/soil_manager.gd
@@ -5,8 +5,8 @@ var x
 var y
 func _ready() -> void:
 	
-	for y in range(-30,144,+15):
-		for x in range(188, -197, -16):
+	for y in range(512,768,+15):
+		for x in range(384, -320, -15):
 		
 			var soil=SOIL.instantiate()
 			soil.position=Vector2(x,y)
