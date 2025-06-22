6a29f51ebb4560e5838acd1a90b1f186dd0fc01f
Anishax11 <anishabhosale11@gmail.com>
2025-06-23 02:50:05 +0530

    Added click and drag function for items in inventory

--- scripts/panel.gd
+++ scripts/panel.gd
@@ -1,14 +1,21 @@
 extends Panel
 
 @onready var texture_rect: TextureRect = $TextureRect
-	
+var button_held=false	
 
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT :   
 		if event.pressed:
-			#print("Clicked panel")
+			print("Clicked panel")
+			button_held=true
 			#print(texture_rect)
 			Global.panel_clicked=!Global.panel_clicked
 		elif ! event.pressed:
+			button_held=false
 			print("Left button")
+	
+	if event is InputEventMouseMotion and button_held==true:
+		print("Dragging")
+		texture_rect.position=get_local_mouse_position()
+		print(texture_rect.global_position)
