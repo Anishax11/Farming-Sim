45661ccd6f0edc8688e51130afd1d950658738ab
Anishax11 <anishabhosale11@gmail.com>
2025-06-23 04:37:13 +0530

    Enabled click and drag for inventory items

--- scripts/panel.gd
+++ scripts/panel.gd
@@ -3,6 +3,8 @@ extends Panel
 @onready var texture_rect: TextureRect = $TextureRect
 var button_held=false	
 
+func _ready():
+	self.connect("gui_input", Callable(self, "_on_gui_input"))
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_LEFT :   
