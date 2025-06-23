16859ddb6f7dd88ac6204402fc89a48a54032484
Anishax11 <anishabhosale11@gmail.com>
2025-06-23 21:40:26 +0530

    Added code to track slot number of item

--- scripts/panel.gd
+++ scripts/panel.gd
@@ -2,7 +2,7 @@ extends Panel
 
 @onready var texture_rect: TextureRect = $TextureRect
 var button_held=false	
-
+var panel_number
 func _ready():
 	self.connect("gui_input", Callable(self, "_on_gui_input"))
 
@@ -11,12 +11,22 @@ func _on_gui_input(event: InputEvent) -> void:
 		if event.pressed:
 			print("Clicked panel")
 			button_held=true
-			#print(texture_rect)
+			#print("ini pos:",texture_rect.position)
+			#print("self:",self.position)
 			Global.panel_clicked=!Global.panel_clicked
 		elif ! event.pressed:
 			button_held=false
-			print("Left button")
-	
+			#Get panel no
+			var text = self.name
+			var regex = RegEx.new()
+			regex.compile(r"\d+")  # Matches one or more digits
+			var result = regex.search(text)
+			panel_number=int(result.get_string(0))
+			print(panel_number)
+			#print("panels moved x:", round(texture_rect.position.x / 40))
+			var final_panel=round(texture_rect.position.y / 40)*5+round(texture_rect.position.x / 40)+panel_number
+			print("Finale panel:",final_panel)
+			
 	if event is InputEventMouseMotion and button_held==true:
 		print("Dragging")
 		texture_rect.position=get_local_mouse_position()
