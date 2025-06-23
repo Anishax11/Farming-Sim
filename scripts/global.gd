e0f59d0584d3c1fa2ab758b5e46f5a32abb195ea
Anishax11 <anishabhosale11@gmail.com>
2025-06-23 22:25:37 +0530

    Enabled click and drag of items in inventory

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,6 +4,9 @@ var grass_clicked=false
 var soil_clicked=false
 var panel_clicked=false
 var player_direction
+var panel_number
+
+
 #func till_soil(player):
 	
 	#if abs(position-location)<=16:
@@ -13,3 +16,15 @@ var player_direction
 func get_direction(direction) :
 	player_direction=direction
 	#print("player_direction",player_direction)
+
+func move_item(panel_number):
+	var texture_rect=get_node("/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel" +str(panel_number)+"/TextureRect")
+	print("INI:","/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel" +str(panel_number)+"/TextureRect")
+	get_node("/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel" +str(panel_number)).remove_child(texture_rect)
+	#print("panels moved x:", round(texture_rect.position.x / 40))
+	var final_panel=round(texture_rect.position.y / 40)*5+round(texture_rect.position.x / 40)+panel_number
+	print("Finale panel:",final_panel)
+	get_node("/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel"+str(final_panel)).add_child(texture_rect)
+	print(texture_rect.get_path())
+	texture_rect.global_position=get_node("/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel"+str(final_panel)).global_position
+	print(get_node("/root/Game/Farmer/Inventory/NinePatchRect/GridContainer/Panel"+str(final_panel)).position)
--- scripts/panel.gd
+++ scripts/panel.gd
@@ -1,6 +1,6 @@
 extends Panel
 
-@onready var texture_rect: TextureRect = $TextureRect
+#@onready var texture_rect: TextureRect = $TextureRect
 var button_held=false	
 var panel_number
 func _ready():
@@ -13,21 +13,21 @@ func _on_gui_input(event: InputEvent) -> void:
 			button_held=true
 			#print("ini pos:",texture_rect.position)
 			#print("self:",self.position)
+			#print(texture_rect.get_path())
 			Global.panel_clicked=!Global.panel_clicked
 		elif ! event.pressed:
 			button_held=false
-			#Get panel no
+			
 			var text = self.name
 			var regex = RegEx.new()
 			regex.compile(r"\d+")  # Matches one or more digits
 			var result = regex.search(text)
 			panel_number=int(result.get_string(0))
 			print(panel_number)
-			#print("panels moved x:", round(texture_rect.position.x / 40))
-			var final_panel=round(texture_rect.position.y / 40)*5+round(texture_rect.position.x / 40)+panel_number
-			print("Finale panel:",final_panel)
+			Global.move_item(panel_number)
 			
 	if event is InputEventMouseMotion and button_held==true:
 		print("Dragging")
-		texture_rect.position=get_local_mouse_position()
-		print(texture_rect.global_position)
+		if $TextureRect!=null:
+			$TextureRect.position=get_local_mouse_position()
+			print($TextureRect.global_position)
