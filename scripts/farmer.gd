01bf5642de06a03b5b888028a4ad7e63fa651b74
Anishax11 <anishabhosale11@gmail.com>
2025-06-25 20:21:26 +0530

    Fixed player movement

--- scripts/farmer.gd
+++ scripts/farmer.gd
@@ -11,37 +11,38 @@ var direction: Vector2
 func _physics_process(delta: float) -> void:
 	
 	if direction==Vector2(0,1):
-		rotation_degrees=0
+		#rotation_degrees=0
 		animated_sprite_2d.play("forward")
 	if direction==Vector2(0,-1):
-		rotation_degrees=0
+		#rotation_degrees=0
 		animated_sprite_2d.play("backward")
 	if direction==Vector2(-1,0):
-		rotation_degrees=0
+		#rotation_degrees=0
 		animated_sprite_2d.play("left")
 	if direction==Vector2(1,0):
 		animated_sprite_2d.stop
-		rotation_degrees=0
+		#rotation_degrees=0
 		animated_sprite_2d.play("right")	
 		
 	if direction.y==1:
-		animated_sprite_2d.play("forward")
+		
 		if direction.x==-1:
-			rotation_degrees=45
+			animated_sprite_2d.play("left")
 			#print("MOving down left")
 		if direction.x==1:
-			rotation_degrees=-45
+			
+			animated_sprite_2d.play("right")
 			#print("MOving down right")
 			
 
 	elif direction.y==-1:
-		animated_sprite_2d.play("backward")
-		animated_sprite_2d.stop
+		
+		
 		if direction.x==-1:
-			rotation_degrees=-45
+			animated_sprite_2d.play("left")
 			#print("MOving up left")
 		if direction.x==1:
-			rotation_degrees=45
+			animated_sprite_2d.play("right")
 			#print("MOving up right")
 
 	#elif direction.x==1:
