2d083b1c92c028838be7a325c1f65cd9a82eacd1
Anishax11 <anishabhosale11@gmail.com>
2025-07-08 18:07:19 +0530

    Adjusted node heirarchy so time and date display follows player

--- scripts/door.gd
+++ scripts/door.gd
@@ -6,7 +6,7 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and  event.button_index == MOUSE_BUTTON_RIGHT:
 		if event.pressed:
 			print("Door clicked")
-			var time_manager=get_node("/root/Game/farm_scene/TimeManager")
+			var time_manager=get_node("/root/Game/farm_scene/Farmer/TimeManager")
 			Global.track_time(time_manager.current_time,time_manager.time_to_change_tint,time_manager.color_rect.i)
 			get_tree().change_scene_to_packed(interior)
 			
