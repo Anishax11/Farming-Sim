a470bc42f56f88811720e2eed42e401334c8a359
Anishax11 <anishabhosale11@gmail.com>
2026-01-10 20:47:25 +0530

    Enable page content display for book

--- scripts/plant_book.gd
+++ scripts/plant_book.gd
@@ -2,9 +2,28 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var current_page = 0
-var total_pages = 5
+var total_pages = 10
+@onready var right: RichTextLabel = $RightPage/Right
+@onready var left: RichTextLabel = $LeftPage/Left
+var offset = -1
 @onready var back: Button = $Back
+var content = {
+	page1 ="Excerpt from “On Cultivated Flora and Their Comforts”
+(Author unknown, margins annotated in a steady, analytical hand)",
 
+	page2 ="Strawberry Ideal Comfort Range: ~21°C
+Strawberries are temperamental not because ",
+
+	page3 ="they are fragile, but because they are honest. They reflect",
+
+	page4 =" their surroundings without restraint. At approximately twenty-one degrees, the plant settles into a quiet productivity — leaves widen, sugar concentrates, and growth remains steady rather than frantic.Below this, sweetness dulls. Above it, the fruit rushes and hollows.",
+	page5 ="Pageeeeeeeeee5",
+	page6 ="Pageeeeeeeeee6",
+	page7 ="Pageeeeeeeeee7",
+	page8 ="Pageeeeeeeeee8",
+	page9 ="Pageeeeeeeeee9",
+	page10 ="Pageeeeeeeeee10"
+}
 func flip_to_next():
 	
 	if current_page == 0: #book closed
@@ -22,7 +41,14 @@ func flip_to_next():
 	else:
 		animated_sprite_2d.play("middle_page_front")
 	current_page+=1
-	print("Page no:",current_page)
+	if current_page>1:
+		left.text = content["page"+str(current_page+offset)]
+		if content.has("page"+str(current_page+1+offset)):
+			right.text = content["page"+str(current_page+1+offset)]
+	elif current_page == 1:
+		right.text = content["page"+str(current_page)]
+	offset+=1
+	#print("Page no:",current_page)
 
 func flip_back():
 	
@@ -36,7 +62,16 @@ func flip_back():
 	else:
 		animated_sprite_2d.play("middle_page_back")
 	current_page-=1
-	print("Page no:",current_page)
+	offset-=1
+	if current_page>1:
+		left.text = content["page"+str(current_page+offset-1)]
+		if content.has("page"+str(current_page+1+offset)):
+			right.text = content["page"+str(current_page+1+offset-1)]
+	elif current_page == 1:
+		right.text = content["page"+str(current_page)]
+		left.text=""
+	
+	#print("Page no:",current_page)
 
 func _on_next_button_down() -> void:
 	flip_to_next()
