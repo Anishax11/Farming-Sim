68a636c92ccc32095c91bb3024a63e68a25e7c49
Anishax11 <anishabhosale11@gmail.com>
2026-01-17 17:39:10 +0530

    Improve farming tutorial

--- scripts/church.gd
+++ scripts/church.gd
@@ -1,10 +1,12 @@
 extends Area2D
-const CHURCH_INTERIOR = preload("res://scenes/church_interior.tscn")
+var CHURCH_INTERIOR = load("res://scenes/church_interior.tscn")
 var time_manager
 var Dim_bg
 func _ready() -> void:
+	
 	time_manager = get_tree().current_scene.find_child("TimeManager",true,false)
 	Dim_bg = get_parent().get_node("Farmer/CanvasLayer2/DimBG")
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body.name!="Farmer" :
 		return
@@ -21,6 +23,7 @@ func _on_body_entered(body: Node2D) -> void:
 				#print("Complete registrations")
 				#return
 		#Global.player_pos = Vector2(-214, -187)
+	print(self.name)
 	Global.track_time(time_manager.current_time,time_manager.time_to_change_tint,time_manager.color_rect.i,time_manager.minutes)
 	Global.music_fade_out()
 	Dim_bg.dim_bg(CHURCH_INTERIOR)
