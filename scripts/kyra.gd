01f7138b3fc47fddf9a0cead7ff63894d9621e6f
Anishax11 <anishabhosale11@gmail.com>
2026-01-24 21:59:35 +0530

    Complete kyra task

--- scripts/kyra.gd
+++ scripts/kyra.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+var sucess_rate = 0
 
 #func _ready() -> void:
 	#if Global.day_count<5:
@@ -9,10 +9,19 @@ extends CharacterBody2D
 
 func _on_input_detector_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	Dialogic.VAR.set("talk_to_bullies_given",TaskManager.tasks["Task10"]["acquired"])
+	Dialogic.VAR.set("talk_to_bullies_given",true)
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 	Dialogic.start("Kyra")
 
 func _on_dialogic_signal(arg : String):
-	if arg =="talked_to_bully":
+	if arg =="increment":
+		sucess_rate+=1
+		
+	
+	if arg =="decrement":
+		sucess_rate-=1
+		Dialogic.VAR.set("kyra_sucess_rate",sucess_rate)
+		
+	if arg =="talked_to_bully" and sucess_rate>=3:
 		TaskManager.tasks["Task10"]["completed"]=true
 		get_tree().get_current_scene().find_child("TaskManager",true,false).remove_task("Task10")
